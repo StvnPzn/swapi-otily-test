ef2768f217485fa6612a89b7cf0ac2f94dab59fc
Steven Pouzon <spouzon@gmail.com>
2021-08-05 15:42:31 +0200

    routes, controller and hompepage basic design

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+
+  resources :films, only: [:index, :show]
+  resources :planets, only: [:index, :show]
+  resources :species, only: [:index, :show]
+  resources :starships, only: [:index, :show]
+  resources :vehicles, only: [:index, :show]
+  resources :characters, only: [:index, :show]
 end
